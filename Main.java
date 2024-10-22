efd23f3f499cd15147d522c11da5b896a7cd8fbb
UtkuAkkusoglu <utku.akkusoglu2004@gmail.com>
2024-10-22 19:38:45 +0300

    Main comments

--- Main.java
+++ Main.java
@@ -8,12 +8,12 @@ public class Main {
         Visuals visual = new Visuals();
         System.out.println(levels.size());
         int i = 1;
-        for (var level : levels) {        // creates a game board for each level and visualizes if a solution is exist.
+        for (var level : levels) {        // Creates a game board for each level
             Board board = new Board(level);
-            State state = new State(board, null, null);
+            State state = new State(board, null, null);  // Initial state in the level
             BFS solver = new BFS();
-            ArrayList<State> path = solver.bfs(state);
-            if (path != null) {
+            ArrayList<State> path = solver.bfs(state); 
+            if (path != null) {           //  Visualizes if a solution is exist
                 System.out.println(path.size());
                 visual.visualizePath(path, i);
             }
